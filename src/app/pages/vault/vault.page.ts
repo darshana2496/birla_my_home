15409662acc05859bd574bd3d708cb84b79b03a0
darshana2496 <darshana@digitalsalt.in>
2024-02-27 12:07:18 +0530

    vault page changes

--- src/app/pages/vault/vault.page.ts
+++ src/app/pages/vault/vault.page.ts
@@ -30,11 +30,6 @@ export class VaultPage implements OnInit {
   isSlideOpen: Boolean = false;
   adminUploadedDocs: ITdsDocuments[] = [];
   userUploadedDocs: ITdsDocuments[] = [];
-  staticVaultList: IVaultDocumentType[] = [
-    { name: 'Demand Letters', count: 0, documentList: '' },
-    { name: 'Payment Receipts', count: 0, documentList: '' },
-    { name: 'Other Property Letters', count: 0, documentList: '' },
-  ];
   adminHandoverDocs: any;
   adminMiscellaneousDocs: any;
   constructor(
@@ -47,17 +42,6 @@ export class VaultPage implements OnInit {
     this.vaultBirlaUploads();
     this.activeItemSliding = null;
     this.isSlideOpen = false;
-    this.resetListCount();
-    this.pageReload();
-  }
-
-  pageReload() {
-    this.globalService.eventsample.subscribe((x) => {
-      console.log(x);
-      if (x) {
-        this.vaultBirlaUploads();
-      }
-    });
   }
 
   vaultBirlaUploads() {
@@ -76,158 +60,74 @@ export class VaultPage implements OnInit {
         if (
           response.btIsSuccess &&
           response.object 
-          // response.object.subPkgDocumentList &&
-          // response.object.subPkgHandoverDocumentList &&
-          // response.object.subPkgHandoverDocumentList
         ) {
 
-          this.adminUploadedDocs = response.object.subPkgDocumentList ? response.object.subPkgDocumentList : [];
-          this.adminHandoverDocs = response.object.subPkgHandoverDocumentList ? response.object.subPkgHandoverDocumentList: [];
-          this.adminMiscellaneousDocs = response.object.subPkgMiscDocumentList ? response.object.subPkgMiscDocumentList: [];
+          this.adminUploadedDocs = response.object.subPkgDocumentList.length > 0 ? response.object.subPkgDocumentList : [];
+          this.adminHandoverDocs = response.object.subPkgHandoverDocumentList.length > 0 ? response.object.subPkgHandoverDocumentList: [];
+          this.adminMiscellaneousDocs = response.object.subPkgMiscDocumentList.length > 0 ? response.object.subPkgMiscDocumentList: [];
 
-          //if (this.globalService.tdsFilterObject.documentType == "") {
-          for (let i = 0; i < response.object.subPkgDocumentList.length; i++) {
-            let decryptVcType = this.globalService.decrypt(
-              this.globalService.encryptSecretKey,
-              response.object.subPkgDocumentList[i].vcType
-            );
-            let decryptvcFilename = this.globalService.decrypt(
-              this.globalService.encryptSecretKey,
-              response.object.subPkgDocumentList[i].vcFilename
-            );
-            let decryptvcFileURL = this.globalService.decrypt(
-              this.globalService.encryptSecretKey,
-              response.object.subPkgDocumentList[i].vcFileUrl
-            );
+          if(this.adminUploadedDocs.length > 0) {
+            this.adminUploadedDocs.map((item)=> {
+              item.vcType = this.globalService.decrypt(
+                this.globalService.encryptSecretKey,
+                item.vcType
+              );
+              item.vcFilename = this.globalService.decrypt(
+                this.globalService.encryptSecretKey,
+                item.vcFilename
+              );
+              item.vcFileUrl = this.globalService.decrypt(
+                this.globalService.encryptSecretKey,
+                item.vcFileUrl
+              );
+  
+            })
 
-            response.object.subPkgDocumentList[i].vcType = decryptVcType;
-            response.object.subPkgDocumentList[i].vcFilename =
-              decryptvcFilename;
-            response.object.subPkgDocumentList[i].vcFileUrl = decryptvcFileURL;
-
-            let obj = response.object.subPkgDocumentList[i];
-            let count = -1;
-            for (let j = 0; j < this.staticVaultList.length; j++) {
-              const element = this.staticVaultList[j];
-
-              if (element.name == obj.vcType) {
-                count = i;
-                element.count += 1;
-                let newObj = obj.vcFilename + ' ';
-                element.documentList += element.documentList + newObj;
-              }
-            }
-            if (count == -1) {
-              //new type other then the three static onces
-              let newObj = obj.vcFilename;
-              let newStaticVal = {
-                name: obj.vcType,
-                count: 1,
-                documentList: newObj,
-              };
-              this.staticVaultList.push(newStaticVal);
-            }
-            
-            
+            this.adminUploadedDocs = this.groupBy(this.adminUploadedDocs, 'vcType');
           }
-          let groupedArray = this.groupBy(response.object.subPkgDocumentList, 'vcType');
-          this.adminUploadedDocs = groupedArray
-          console.log("***********document filter********", this.adminUploadedDocs)
-            
 
-          // handover docs decryption
-          for (
-            let i = 0;
-            i < response.object.subPkgHandoverDocumentList.length;
-            i++
-          ) {
-            let decryptVcType = this.globalService.decrypt(
-              this.globalService.encryptSecretKey,
-              response.object.subPkgHandoverDocumentList[i].vcType
-            );
-            let decryptvcFilename = this.globalService.decrypt(
-              this.globalService.encryptSecretKey,
-              response.object.subPkgHandoverDocumentList[i].vcFilename
-            );
-            let decryptvcFileURL = this.globalService.decrypt(
-              this.globalService.encryptSecretKey,
-              response.object.subPkgHandoverDocumentList[i].vcFileUrl
-            );
-
-            response.object.subPkgHandoverDocumentList[i].vcType =
-              decryptVcType;
-            response.object.subPkgHandoverDocumentList[i].vcFilename =
-              decryptvcFilename;
-            response.object.subPkgHandoverDocumentList[i].vcFileUrl =
-              decryptvcFileURL;
-
-            // let obj = response.object.subPkgHandoverDocumentList[i];
-            // let count = -1;
-            // for (let j = 0; j < this.staticVaultList.length; j++) {
-            //   const element = this.staticVaultList[j];
-
-            //   if (element.name == obj.vcType) {
-            //     count = i;
-            //     element.count += 1;
-            //     let newObj = obj.vcFilename + ' ';
-            //     element.documentList += element.documentList + newObj;
-            //   }
-            // }
-            // if (count == -1) {
-            //   //new type other then the three static onces
-            //   let newObj = obj.vcFilename;
-            //   let newStaticVal = {
-            //     name: obj.vcType,
-            //     count: 1,
-            //     documentList: newObj,
-            //   };
-            //   this.staticVaultList.push(newStaticVal);
-            // }
+          if(this.adminHandoverDocs.length > 0){
+            this.adminHandoverDocs.map((item)=> {
+              item.vcType = this.globalService.decrypt(
+                this.globalService.encryptSecretKey,
+               item.vcType
+              );
+              item.vcFilename = this.globalService.decrypt(
+                this.globalService.encryptSecretKey,
+               item.vcFilename
+              );
+              item.vcFileUrl = this.globalService.decrypt(
+                this.globalService.encryptSecretKey,
+               item.vcFileUrl
+              );
+            })
           }
          
-          // Miscellaneous docs decryption
-          for (
-            let i = 0;
-            i < response.object.subPkgMiscDocumentList.length;
-            i++
-          ) {
-            let decryptVcType = this.globalService.decrypt(
-              this.globalService.encryptSecretKey,
-              response.object.subPkgMiscDocumentList[i].vcType
-            );
-            let decryptvcFilename = this.globalService.decrypt(
-              this.globalService.encryptSecretKey,
-              response.object.subPkgMiscDocumentList[i].vcFilename
-            );
-            let decryptvcFileURL = this.globalService.decrypt(
-              this.globalService.encryptSecretKey,
-              response.object.subPkgMiscDocumentList[i].vcFileUrl
-            );
 
-            response.object.subPkgMiscDocumentList[i].vcType =
-              decryptVcType;
-            response.object.subPkgMiscDocumentList[i].vcFilename =
-              decryptvcFilename;
-            response.object.subPkgMiscDocumentList[i].vcFileUrl =
-              decryptvcFileURL;
-
+          if(this.adminMiscellaneousDocs.length > 0){
+            // Miscellaneous docs decryption
+            this.adminMiscellaneousDocs.map((item)=> {
+              item.vcType = this.globalService.decrypt(
+                this.globalService.encryptSecretKey,
+                item.vcType
+              );
+              item.vcFilename = this.globalService.decrypt(
+                this.globalService.encryptSecretKey,
+                item.vcFilename
+              );
+              item.vcFileUrl = this.globalService.decrypt(
+                this.globalService.encryptSecretKey,
+                item.vcFileUrl
+              );
+            })
           }
         } else {
           this.adminUploadedDocs = [];
           this.adminHandoverDocs = [];
           this.adminMiscellaneousDocs = [];
         }
-        console.log("Document list", this.adminUploadedDocs)
       })
       .catch((response: any) => {});
-  }
-
-  resetListCount() {
-    this.staticVaultList = [
-      { name: 'Demand Letters', count: 0, documentList: '' },
-      { name: 'Payment Receipts', count: 0, documentList: '' },
-      { name: 'Other Property Letters', count: 0, documentList: '' },
-    ];
   }
 
   getList(key, arr) {
